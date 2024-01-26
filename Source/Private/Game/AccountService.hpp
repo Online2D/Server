c0b22f8f01fcfd7c28bc056e7b3c134f808b5696
Wolftein <Wolftein@users.noreply.github.com>
2024-01-26 19:54:51 -0300

    Updated license information.

--- Source/Private/Game/AccountService.hpp
+++ Source/Private/Game/AccountService.hpp
@@ -1,5 +1,5 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Copyright (C) 2024 by Agustin Alvarez. All rights reserved.
+// Copyright (C) 2024 by Online-MMO-Engine Team. All rights reserved.
 //
 // This work is licensed under the terms of the MIT license.
 //
@@ -26,13 +26,13 @@ namespace Game
     public:
 
         // -=(Undocumented)=-
-        virtual void Create(ConstSPtr<Account> Account) = 0;
+        virtual Bool Create(ConstSPtr<Account> Account) = 0;
 
         // -=(Undocumented)=-
-        virtual void Delete(UInt ID) = 0;
+        virtual Bool Delete(UInt ID) = 0;
 
         // -=(Undocumented)=-
-        virtual void Update(ConstSPtr<Account> Account) = 0;
+        virtual Bool Update(ConstSPtr<Account> Account) = 0;
 
         // -=(Undocumented)=-
         virtual SPtr<Account> GetByID(UInt ID) const = 0;
